f547ff65b985454ca259bc99e343f0a47999bc77
Yaroslav Sidorov <Macfinch@Macfinch.home>
2019-06-25 19:13:23 -0400

    Adding bootstrap styles

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/test", {useNewUrlParser: true});
 const itemSchema = { number: String, date: String };
 const Item = mongoose.model("Item", itemSchema);
 
