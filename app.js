1aeb86d4dcee55aa573a53c719d4b6ab4b44dbfd
Yaroslav Sidorov <yaroslavvsidorov@icloud.com>
2019-06-24 20:21:08 -0400

    PDF Generator Implement

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const mongoose = require("mongoose");
 const _=require("lodash");
 const app = express();
 
-const PDFDocument = require('pdfkit');
-const doc = new PDFDocument;
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -14,16 +11,44 @@ mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}
 const itemSchema = { number: String, date: String };
 const Item = mongoose.model("Item", itemSchema);
 
+
+// Define font files
+var fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+  }
+};
+
+var PdfPrinter = require('pdfmake/src/printer');
+var printer = new PdfPrinter(fonts);
+var fs = require('fs');
+var options = {}
+
 app.get("/", function(req, res) {
   Item.find({}, function(err, dbItems){ 
-    console.log(dbItems);
     res.render("home", {dbItems: dbItems});
   });
 });
 
 app.get("/:itemNumber", function(req, res) {
   Item.findOne({number: req.params.itemNumber}, function(err, foundItem){
-    res.render("item", {item: foundItem});
+   var docDefinition = {
+     content: [
+       foundItem.number,
+       foundItem.date
+      ]
+    };
+    var createFile = function() {
+      var pdfDoc = printer.createPdfKitDocument(docDefinition, options);
+        pdfDoc.pipe(fs.createWriteStream("public/" + foundItem.number + ".pdf")).on("finish", function(){
+        console.log("File Created");
+      });
+      pdfDoc.end();
+    }
+    res.render("item", {item: foundItem, createPDF: createFile});
   });
 });
 
