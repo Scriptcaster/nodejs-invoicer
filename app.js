1eca554eccfbb5f3d8a1e2523a229eebb8f543fb
Yaroslav Sidorov <yaroslavvsidorov@icloud.com>
2019-07-02 21:25:41 -0400

    Add Heroku Port

--- app.js
+++ app.js
@@ -134,11 +134,13 @@ app.post("/delete", function(req, res) {
   });
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+app.listen(process.env.PORT);
 
-app.listen(port, function() {
-  console.log("Server started on port 3000");
-});
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
+
+// app.listen(port, function() {
+//   console.log("Server started on port 3000");
+// });
